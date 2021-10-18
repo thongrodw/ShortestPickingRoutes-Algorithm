117f068fa3cafab27ea8f5112c0d8f1aba3d8265
Wongsatorn Thongrod <thongrodw@gmail.com>
2021-10-18 13:25:30 +0700

    update

--- services/index.js
+++ services/index.js
@@ -2,18 +2,10 @@ const {Graph} = require('../utils/graph')
 
 //Shortest Distance Node
 let shortestDistanceNode = (distances, visited) => {
-    // create a default value for shortest
     let shortest = null;
-    
-    // for each node in the distances object
     for (let node in distances) {
-        // if no node has been assigned to shortest yet
-        // or if the current node's distance is smaller than the current shortest
         let currentIsShortest = shortest === null || distances[node] < distances[shortest];
-            
-        // and if the current node is in the unvisited set
         if (currentIsShortest && !visited.includes(node)) {
-            // update shortest to be the current node
             shortest = node;
         }
     }
